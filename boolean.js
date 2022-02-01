98a9aaba50c36c9cd56ef293ab3d26f6bccd8eb0
Maureen-max <77680352+Maureen-max@users.noreply.github.com>
2022-02-01 23:02:30 +0300

    Javascript Day One Challenge

---
+++ boolean.js
@@ -0,0 +1,20 @@
+const NumberA = 10;
+const NumberB = 20;
+var result = NumberA + NumberB;
+document.write("The results of addition : " + result);
+document.write("<br>");
+
+var result = NumberA - NumberB;
+document.write("The results of subtraction : " + result);
+document.write("<br>");
+
+var result = NumberA * NumberB;
+document.write("The results of multiplication : " + result);
+document.write("<br>");
+
+var result = NumberA % NumberB;
+document.write("The results of modulus : " + result);
+document.write("<br>");
+
+var result = NumberA / NumberB;
+document.write("The results of division : " + result);
